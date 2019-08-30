da5627bf6a3dce3686719143e266f06068442513
Brendan Fallon <brendanc.fallon@gmail.com>
2019-08-30 08:03:07 -0400

    fixes deploy issue

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const cors = require('cors')
 
 
+
 router.use(cors())
 /* GET home page. */
-router.use('/comments', require('./comments'));
+
+router.use('/', require('./comments'))
+// router.use('/comments', comments);
 
 
 module.exports = router;
