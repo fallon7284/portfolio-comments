5a6deccf904cf81e167019be212f8eb8684277d5
Brendan Fallon <brendanc.fallon@gmail.com>
2019-09-03 18:42:30 -0400

    please connect to pg

--- db/database.js
+++ db/database.js
@@ -6,9 +6,17 @@ const pkg = require('../package.json')
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
 console.log(chalk.yellow(`Opening database connection to ${dbName}`))
 
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
-  logging: false,
-  operatorsAliases: false,
-})
+// const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
+//   logging: false,
+//   operatorsAliases: false,
+// })
+
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+      ssl: true
+  }
+});
 
 module.exports = db
